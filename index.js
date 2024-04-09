4c1378c75e578820ae182e79619925516f18cb4f
Timur Kazbekov <tkazbekov@gmail.com>
2024-04-09 17:57:44 +0300

    export a properly formated csv

--- index.js
+++ index.js
@@ -1,66 +1,106 @@
-const fs = require('fs');
-const path = require('path');
-const { chromium } = require('playwright');
-const { exit } = require('process');
+const fs = require("fs");
+const path = require("path");
+const { chromium } = require("playwright");
+const { exit } = require("process");
+const { stringify } = require("querystring");
 
 const [url, file] = process.argv.slice(2);
 
 if (!url) {
-    throw 'Please provide a URL as the first argument.';
+  throw "Please provide a URL as the first argument.";
 }
 
-async function run() {
-    const browser = await chromium.launch();
-    const page = await browser.newPage();
+async function scrape() {
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
 
-    await page.goto(url);
-    await page.waitForSelector('.easy-card-list');
+  await page.goto(url);
+  await page.waitForSelector(".easy-card-list");
 
-    const boardTitle = await page.$eval('.board-name', (node) => node.innerText.trim());
+  const boardName = await page.$eval(".board-name", (node) =>
+    node.innerText.trim()
+  );
+  const columnElements = await page.$$(".easy-card-list");
 
-    if (!boardTitle) {
-        throw 'Board title does not exist. Please check if provided URL is correct.'
-    }
+  const board = {
+    name: boardName,
+    columns: await Promise.all(
+      columnElements.map(async (column) => {
+        const columnName = await column.$eval(".column-header", (node) =>
+          node.innerText.trim()
+        );
+        const messages = await column.$$(".easy-board-front");
 
-    let parsedText = boardTitle + '\n\n';
+        return {
+          name: columnName,
+          messages: await Promise.all(
+            messages.map(async (message) => {
+              const text = await message.$eval(
+                ".easy-card-main .easy-card-main-content .text",
+                (node) => node.innerText.trim()
+              );
+              const votes = await message.$eval(
+                ".easy-card-votes-container .easy-badge-votes",
+                (node) => node.innerText.trim()
+              );
 
-    const columns = await page.$$('.easy-card-list');
+              return { text, votes };
+            })
+          ),
+        };
+      })
+    ),
+  };
 
-    for (let i = 0; i < columns.length; i++) {
-        const columnTitle = await columns[i].$eval('.column-header', (node) => node.innerText.trim());
-
-        const messages = await columns[i].$$('.easy-board-front');
-        if (messages.length) {
-            parsedText += columnTitle + '\n';
-        }
-        for (let i = 0; i < messages.length; i++) {
-            const messageText = await messages[i].$eval('.easy-card-main .easy-card-main-content .text', (node) => node.innerText.trim());
-            const votes = await messages[i].$eval('.easy-card-votes-container .easy-badge-votes', (node) => node.innerText.trim());
-            parsedText += `- ${messageText} (${votes})` + '\n';
-        }
-
-        if (messages.length) {
-            parsedText += '\n';
-        }
-    }
-
-    return parsedText;
+  return board;
 }
+
+function formatBoardToText(board) {
+  return [
+    board.name,
+    ...board.columns.flatMap((column) => [
+      `${column.name}:`,
+      ...column.messages.map(({ text, votes }) => `${text} (${votes})`),
+      "",
+    ]),
+  ].join("\n");
+}
+
+function formatBoardToCsv(board) {
+  const columnHeaders = board.columns.map(column => column.name);
+  const rows = board.columns.map(column => column.messages);
+
+  const maxMessages = Math.max(...rows.map(messages => messages.length));
+
+  let csvContent = columnHeaders.join(",") + "\n";
+
+  for (let i = 0; i < maxMessages; i++) {
+    csvContent += rows
+      .map(messages => `"${messages[i]?.text || ""}"`)
+      .join(",") + "\n";
+  }
+
+  return csvContent;
+}
+
 
 function writeToFile(filePath, data) {
-    const resolvedPath = path.resolve(filePath || `../${data.split('\n')[0].replace('/', '')}.txt`);
-    fs.writeFile(resolvedPath, data, (error) => {
-        if (error) {
-            throw error;
-        } else {
-            console.log(`Successfully written to file at: ${resolvedPath}`);
-        }
-        process.exit();
-    });
+  const resolvedPath = path.resolve(
+    filePath || `../${data.split("\n")[0].replace("/", "")}.txt`
+  );
+  fs.writeFile(resolvedPath, data, (error) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log(`Successfully written to file at: ${resolvedPath}`);
+    }
+    process.exit();
+  });
 }
 
 function handleError(error) {
-    console.error(error);
+  console.error(error);
 }
 
-run().then((data) => writeToFile(file, data)).catch(handleError);
+console.log(`Exporting "${url}" to "${file}"...`);
+scrape().then((data) => writeToFile(file, formatBoardToCsv(data)));
